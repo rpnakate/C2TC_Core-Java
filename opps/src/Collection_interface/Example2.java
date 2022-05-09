5a8231705ec1a692525fe51bc3e37ef391200f93
Admin <Admin@DESKTOP-ESE4D8U>
2022-05-09 08:52:23 +0530

    first commit

---
+++ opps/src/Collection_interface/Example2.java
@@ -0,0 +1,19 @@
+package Collection_interface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example2 {
+	public static void main(String [] args)
+	{ 
+	 List<Integer> val = new ArrayList<>(); 
+	 val.add(0); 
+	 val.add(11); 
+	 val.add(23); 
+	 val.add(34); 
+	 val.add(45);
+	 val.remove(1);
+	 System.out.println("ArrayList : " + val); 
+	} 
+
+}
